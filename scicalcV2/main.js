363f813668b342d6618d16ca331f7ab593ecf385
ababythwumps <michaelaedan@duck.com>
2024-10-23 09:08:49 -0700

    YES EVERYTHING WORKS JUST NEED TO PASTE IN ALL THE FUNCTIONS AND FORMAT THEM AND DEAL MAKE SURE THAT I DON'T ADD FLASK

--- scicalcV2/main.js
+++ scicalcV2/main.js
@@ -1,24 +1,74 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+var display = document.getElementById("result");
+var outwardEquation = "";
+var inwardEquation = "";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+function clearDisplay() {
+  outwardEquation = '';
+  inwardEquation = "";
+  display.innerText = '0';
+}
+
+function deleteLast() {
+  outwardEquation = outwardEquation.slice(0, -1);
+  inwardEquation = inwardEquation.slice(0, -1);
+  display.innerText = outwardEquation || '0';
+}
+
+function appendOperator(operator) {
+  if (/[+\-*/^]$/.test(outwardEquation)) {
+      outwardEquation = outwardEquation.slice(0, -1) + operator;
+      inwardEquation = inwardEquation.slice(0, -1) + operator;
+  } else {
+      outwardEquation += operator;
+      inwardEquation += operator;
+  }
+  display.innerText = outwardEquation;
+}
+
+function appendParenthesis(parenthesis) {
+  if (parenthesis === '(' && /[0-9)]$/.test(inwardEquation)) {
+    inwardEquation += '*';
+  }
+  if (/[)]$/.test(inwardEquation) && /[0-9(]/.test(parenthesis)) {
+    inwardEquation += '*';
+  }
+  outwardEquation += parenthesis;
+  inwardEquation += parenthesis;
+  display.innerText = outwardEquation;
+}
+
+function appendCharacter(character) {
+  outwardEquation += character;
+  //ADD CODE TO CHANGE WHAT THE INWARDEQUATION LOGGING DOES FOR CERTAIN FUNCTIONS--MAYBE. THIS COULD ALSO BE
+  //ONLY FOR OTHER FUNCTIONS THAT ARE SPECIFICALLY FOR ADDING OTHER SPECIAL FUNCIOTNS
+  inwardEquation += character
+  display.innerText = outwardEquation;
+  console.log(outwardEquation)
+}
+
+document.addEventListener('keydown', (event) => {
+  const key = event.key;
+  if (/[0-9]/.test(key)) {
+      appendCharacter(key);
+  } else if (/[+\-*/]/.test(key)) {
+  } else if (/[+\-*/^]/.test(key)) {
+      appendOperator(key);
+  } else if (key === 'Enter') {
+      //calculate();
+  } else if (key === 'Backspace') {
+      deleteLast();
+  } else if (key === 'Escape') {
+      clearDisplay();
+  } else if (key === '(' || key === ')') {
+      appendParenthesis(key);
+  } else if (key === '.') {
+      appendCharacter(key);
+  }
+});
+
+window.appendCharacter = appendCharacter;
+window.clearDisplay = clearDisplay;
+window.deleteLast = deleteLast;
+window.appendOperator = appendOperator;
+window.appendParenthesis = appendParenthesis;
